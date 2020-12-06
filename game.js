4a51e914368e9c96c79e2a68dfdc112b82c06efe
Arpad <solar@hu.inter.net>
2020-12-06 19:48:31 +0100

    cards pairs

--- game.js
+++ game.js
@@ -28,15 +28,35 @@ document.querySelector('.start-stop-btn').addEventListener('click', () => {
     }
 });
 
-// Game
+// Game ................................................................
 
 const cards = document.querySelectorAll('.game-card');
 
+let hasFlippedCard = false;
+let firstCard;
+let secondCard;
+
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+        } else {
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+            }, 00);
+        }
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
-
-
 
